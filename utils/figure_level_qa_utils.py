b07880f18f37033028cb4d7b1eefab80de496c51
jnaiman <jnaiman.illinois.edu>
2025-09-08 18:46:20 -0500

    updating gen figure plots

--- utils/figure_level_qa_utils.py
+++ utils/figure_level_qa_utils.py
@@ -8,6 +8,7 @@ def figure_qa_how_many_panels(data, qa_pairs, return_qa=True, verbose=True,
                                text_persona = None):
 
     #nplots = get_nplots(data)
+    outtag = 'rows/columns'
 
     ### persona of assistant
     text_persona = persona(text=text_persona)
@@ -24,7 +25,7 @@ def figure_qa_how_many_panels(data, qa_pairs, return_qa=True, verbose=True,
         print('')
     # add to pairs
     if return_qa: 
-        qa_pairs['Level 1']['Figure-level questions']['rows/columns'] = {'Q':q, 'A':a1, 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':a1, 
                                                                          'persona':text_persona, 
                                                                          'context':text_context, # context nothing for full figure
                                                                          'question':text_question,
@@ -38,6 +39,7 @@ def figure_qa_plotting_style(data, qa_pairs, return_qa=True, verbose=True,
                               #use_words=True, 
                                #single_figure_flag=True, 
                                text_persona = None):
+    outtag = 'plot style'
 
     ### persona of assistant
     text_persona = persona(text=text_persona)
@@ -54,7 +56,7 @@ def figure_qa_plotting_style(data, qa_pairs, return_qa=True, verbose=True,
         print('')
     # add to pairs
     if return_qa: 
-        qa_pairs['Level 1']['Figure-level questions']['plot style'] = {'Q':q, 'A':answer, 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
                                                                          'persona':text_persona, 
                                                                          'context':text_context, # context nothing for full figure
                                                                          'question':text_question,
@@ -67,6 +69,8 @@ def figure_qa_plotting_style(data, qa_pairs, return_qa=True, verbose=True,
 # Colormaps?
 def figure_qa_colormap(data, qa_pairs, return_qa=True, verbose=True, 
                        text_persona = None):
+    
+    outtag = 'colormap'
     
     ### persona of assistant
     text_persona = persona(text=text_persona)
@@ -83,23 +87,194 @@ def figure_qa_colormap(data, qa_pairs, return_qa=True, verbose=True,
         print('')
     # add to pairs
     if return_qa: 
-        qa_pairs['Level 1']['Figure-level questions']['plot style'] = {'Q':q, 'A':answer, 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
                                                                          'persona':text_persona, 
                                                                          'context':text_context, # context nothing for full figure
                                                                          'question':text_question,
                                                                          'format':text_format, 
                                                                          'notes':"Some of the plot styles don't allow for updates to the colormap for REASONS, so just keep that in mind."}
+        return qa_pairs
+    
+
+# aspect ratio
+def figure_qa_aspect_ratio(data, qa_pairs, return_qa=True, verbose=True, 
+                       text_persona = None):
+    
+    outtag = 'aspect ratio'
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    # context for question
+    text_context = '' # full figure
+
+    text_question = 'What is the aspect ratio of this figure?'
+    text_format = 'You are a helpful assistant, please format the output as a json as {"aspect ratio":""} to store the aspect ratio of the plot.'
+    answer = {"aspect ratio":data['figure']['aspect ratio']}
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+    if verbose:
+        print('QUESTION:', text_question)
+        print('ANSWER:', answer)
+        print('')    
+
+    if return_qa: 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
+                                                                         'persona':text_persona, 
+                                                                         'context':text_context, 
+                                                                         'question':text_question,
+                                                                         'format':text_format}
+        return qa_pairs
 
 
-    # q2 = 'Assuming this is a figure made with matplotlib in Python, what is the colormap that was used?  Examples of matplotlib colormaps are "rainbow" or "Reds".'
-    # q2 += ' You are a helpful assistant, please format the output as a json as {"colormap":""} to store the matplotlib colormap used in the figure.'
-    # a2 = {"colormap":data['figure']['color map']}
-    # if verbose:
-    #     print('QUESTION:', q2)
-    #     print('ANSWER:', a2)
-    #     print('')
-    # # add to pairs
-    # if return_qa: 
-    #     qa_pairs['Level 1']['Figure-level questions']['colormap'] = {'Q':q2, 'A':a2, 
-    #                                                              'notes':"Some of the plot styles don't allow for updates to the colormap for REASONS, so just keep that in mind."}
+
+############ INDIVIDUAL PLOTS IN GENERAL ##############
+
+def q_plot_titles(data, qa_pairs, return_qa=True, verbose=True, 
+                  text_persona = None):
+    outtag = 'titles'
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    ### context for question
+    text_context = '' # full figure
+
+    text_question = 'What are the titles for each figure panel?'
+    text_format = 'Please format the output as a json as {"titles":[]}, where the list is a list of strings of all of the titles. '
+    text_format += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order.'
+    text_format += " If a plot does not have a title, then denote this by an empty string in the list.  Please format any formulas in the title in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
+
+    la = []
+    for k,v in data.items():
+        if 'plot' in k: # is a plot
+            if 'title' not in v:
+                la.append("")
+            else:
+                la.append(v['title']['words'])
+    answer = {"title":la}
+
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+    if verbose:
+        print('QUESTION:', text_question)
+        print('ANSWER:', answer)
+        print('')  
+
+    if return_qa: 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
+                                                                         'persona':text_persona, 
+                                                                         'context':text_context, 
+                                                                         'question':text_question,
+                                                                         'format':text_format}
+        return qa_pairs
+
+
+def q_plot_axis_labels(data, qa_pairs, return_qa=True, verbose=True, 
+                   text_persona=None, axis='x'):
+    outtag = axis + 'labels'
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    ### context for question
+    text_context = '' # full figure
+    ### question
+    text_question = 'What are the x-axis titles for each figure panel?'
+    text_format = 'Please format the output as a json as {"'+axis+'labels":[]}, where the list is a list of strings of all of the '+axis+'-axis titles. '
+    text_format += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order.'
+    text_format += " If a plot does not have an "+axis+"-axis title, then denote this by an empty string in the list.  "
+    text_format += "Please format any formulas in the title in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
+    ### all together
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+    ### labels
+    la = []
+    for k,v in data.items():
+        if 'plot' in k: # is a plot
+            if axis+'label' not in v:
+                la.append("")
+            else:
+                la.append(v[axis+'label']['words'])
+    answer = {axis+"labels":la}
+    if verbose:
+        print('QUESTION:', q)
+        print('ANSWER:', answer)
+    if return_qa: 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
+                                                                         'persona':text_persona, 
+                                                                         'context':text_context, 
+                                                                         'question':text_question,
+                                                                         'format':text_format}
+        return qa_pairs
+    
+
+def q_ticklabels(data, qa_pairs, axis='x', return_qa=True, verbose=True, text_persona=None):
+    outtag = axis + 'tick values'
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    ### context for question
+    text_context = '' # full figure
+    text_question = 'What are the values for each of the tick marks on the '+axis+'-axis?'
+    ### format
+    text_format = 'Please format the output as a json as {"'+axis+'tick values":[[]]}, where each element of the outer list refers to a single panel, '
+    text_format += 'and each inner list is a list of the '+axis+'-axis tick mark values. '
+    text_format += 'If there is a single plot, this should be one element in the outer list, and if there are multiple plots the outer list should be in row-major (C-style) order.'
+    text_format += " If a plot does not have any "+axis+"-axis tick values, then denote this by an empty string in the inner list.  "
+    text_format += "Please format any formulas in the "+axis+"-axis tick values in a Python LaTeX string (for example 'Light $\\\\alpha$')."
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+    ### answer
+    la = []
+    for k,v in data.items():
+        if 'plot' in k: # is a plot
+            if axis+'ticks' not in v:
+                la.append("")
+            else:
+                t = []
+                for l in v[axis+'ticks']:
+                    t.append(l['data'])
+                la.append(t)
+    answer = {axis+"tick values":la}
+    if verbose:
+        print('QUESTION:', q)
+        print('ANSWER:', answer)
+    if return_qa: 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
+                                                                         'persona':text_persona, 
+                                                                         'context':text_context, 
+                                                                         'question':text_question,
+                                                                         'format':text_format}
+        return qa_pairs
+
+
+
+def q_plot_types(data, qa_pairs, plot_types, return_qa=True, use_list=True, 
+                 verbose=True, text_persona=None):
+    outtag = 'plot types'
+    ### persona of assistant
+    text_persona = persona(text=text_persona)
+    ### question
+    text_question = 'What are the plot types for each panel in the figure?'
+    ### context for question
+    text_context = '' # full figure
+    if use_list:
+        text_context = 'Please choose each plot type from the following list: ['
+        for pt in plot_types:
+            text_context += pt + ', '
+        text_context = text_context[:-2] # take off the last bit
+        text_context += '].'
+    ### format
+    text_format = 'Please format the output as a json as {"plot types":[]}, where each element of the list refers to the plot type of a single panel. '
+    text_format += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order. '
+    # full q
+    q = text_persona + " " + text_context + " " + text_question + " " + text_format
+
+    la = []
+    for k,v in data.items():
+        if 'plot' in k: # is a plot
+            if 'type' not in v:
+                la.append("")
+            else:
+                la.append(v['type'])
+    answer = {"plot types":la}
+    if verbose:
+        print('QUESTION:', q)
+        print('ANSWER:', answer)
+    if return_qa: 
+        qa_pairs['Level 1']['Figure-level questions'][outtag] = {'Q':q, 'A':answer, 
+                                                                         'persona':text_persona, 
+                                                                         'context':text_context, 
+                                                                         'question':text_question,
+                                                                         'format':text_format}
         return qa_pairs
--- utils/plot_qa_utils.py
+++ utils/plot_qa_utils.py
@@ -46,28 +46,6 @@ def log_scale_ax(scale_exp = {'min':0.01, 'max':5.0},
 
 
 
-
-
-
-
-
-
-
-# aspect ratio
-def q4(data, qa_pairs, return_qa=True, verbose=True):
-    q1 = 'What is the aspect ratio of this figure?'
-    q1 += ' You are a helpful assistant, please format the output as a json as {"aspect ratio":""} to store the aspect ratio of the plot.'
-    a1 = {"aspect ratio":data['figure']['aspect ratio']}
-    if verbose:
-        print('QUESTION:', q1)
-        print('ANSWER:', a1)
-        print('')
-    # add to pairs
-    if return_qa: 
-        qa_pairs['Level 1']['Figure-level questions']['aspect ratio'] = {'Q':q1, 'A':a1}
-        return qa_pairs
-
-
 # dpi
 def dpi(data, qa_pairs, return_qa=True, verbose=True):
     q1 = 'What is the DPI (dots per square inch) of this figure?'
@@ -86,123 +64,18 @@ def dpi(data, qa_pairs, return_qa=True, verbose=True):
 
 ############ INDIVIDUAL PLOTS IN GENERAL ##############
 
-def q_plot_titles(data, qa_pairs, return_qa=True, verbose=True):
-    q = 'What are the titles for each figure panel?'
-    q += ' You are a helpful assistant, please format the output as a json as {"titles":[]}, where the list is a list of strings of all of the titles.  '
-    q += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order.'
-    q += " If a plot does not have a title, then denote this by an empty string in the list.  Please format any formulas in the title in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
-    la = []
-    for k,v in data.items():
-        if 'plot' in k: # is a plot
-            if 'title' not in v:
-                la.append("")
-            else:
-                la.append(v['title']['words'])
-    a = {"title":la}
-    if verbose:
-        print('QUESTION:', q)
-        print('ANSWER:', a)
-    if return_qa: 
-        qa_pairs['Level 1']['Plot-level questions']['titles'] = {'Q':q, 'A':a}
-        return qa_pairs
-
-
-def q_plot_xlabels(data, qa_pairs, return_qa=True, verbose=True):
-    q = 'What are the x-axis titles for each figure panel?'
-    q += ' You are a helpful assistant, please format the output as a json as {"xlabels":[]}, where the list is a list of strings of all of the x-axis titles.  '
-    q += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order.'
-    q += " If a plot does not have an x-axis title, then denote this by an empty string in the list.  "
-    q += "Please format any formulas in the title in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
-    la = []
-    for k,v in data.items():
-        if 'plot' in k: # is a plot
-            if 'xlabel' not in v:
-                la.append("")
-            else:
-                la.append(v['xlabel']['words'])
-    a = {"xlabels":la}
-    if verbose:
-        print('QUESTION:', q)
-        print('ANSWER:', a)
-    if return_qa: 
-        qa_pairs['Level 1']['Plot-level questions']['xlabels'] = {'Q':q, 'A':a}
-        return qa_pairs
-
-
-def q_plot_ylabels(data, qa_pairs, return_qa=True, verbose=True):
-    q = 'What are the y-axis titles for each figure panel?'
-    q += ' You are a helpful assistant, please format the output as a json as {"ylabels":[]}, where the list is a list of strings of all of the y-axis titles.  '
-    q += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order.'
-    q += " If a plot does not have an y-axis title, then denote this by an empty string in the list.  "
-    q += "Please format any formulas in the title in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
-    la = []
-    for k,v in data.items():
-        if 'plot' in k: # is a plot
-            if 'ylabel' not in v:
-                la.append("")
-            else:
-                la.append(v['ylabel']['words'])
-    a = {"ylabels":la}
-    if verbose:
-        print('QUESTION:', q)
-        print('ANSWER:', a)
-    if return_qa: 
-        qa_pairs['Level 1']['Plot-level questions']['ylabels'] = {'Q':q, 'A':a}
-        return qa_pairs
 
 
 
-def ticklabels(data, qa_pairs, tick_type='x', return_qa=True, verbose=True):
-    q = 'What are the values for each of the tick marks on the '+tick_type+'-axis?'
-    q += ' You are a helpful assistant, please format the output as a json as {"'+tick_type+'tick values":[[]]}, where each element of the outer list refers to a single panel, '
-    q += 'and each inner list is a list of the '+tick_type+'-axis tick mark values. '
-    q += 'If there is a single plot, this should be one element in the outer list, and if there are multiple plots the outer list should be in row-major (C-style) order.'
-    q += " If a plot does not have any "+tick_type+"-axis tick values, then denote this by an empty string in the inner list.  "
-    q += "Please format any formulas in the "+tick_type+"-axis tick values in a Python LaTeX string (for example 'Light $\\\\alpha$'). "
-    la = []
-    for k,v in data.items():
-        if 'plot' in k: # is a plot
-            if tick_type+'ticks' not in v:
-                la.append("")
-            else:
-                t = []
-                for l in v[tick_type+'ticks']:
-                    t.append(l['data'])
-                la.append(t)
-    a = {tick_type+"tick values":la}
-    if verbose:
-        print('QUESTION:', q)
-        print('ANSWER:', a)
-    if return_qa: 
-        qa_pairs['Level 1']['Plot-level questions'][tick_type+'tick values'] = {'Q':q, 'A':a}
-        return qa_pairs
 
 
 
-def q_plot_types(data, qa_pairs, plot_types, return_qa=True, use_list=True, verbose=True):
-    q = 'What are the plot types for each panel in the figure? '
-    q += 'You are a helpful assistant, please format the output as a json as {"plot types":[]}, where each element of the list refers to the plot type of a single panel. '
-    q += 'If there is a single plot, this should be one element in this list, and if there are multiple plots the list should be in row-major (C-style) order. '
-    if use_list:
-        q += 'Please choose each plot type from the following list: ['
-        for pt in plot_types:
-            q += pt + ', '
-        q = q[:-2] # take off the last bit
-        q += '].'
-    la = []
-    for k,v in data.items():
-        if 'plot' in k: # is a plot
-            if 'type' not in v:
-                la.append("")
-            else:
-                la.append(v['type'])
-    a = {"plot types":la}
-    if verbose:
-        print('QUESTION:', q)
-        print('ANSWER:', a)
-    if return_qa: 
-        qa_pairs['Level 1']['Plot-level questions']['plot types'] = {'Q':q, 'A':a}
-        return qa_pairs
+
+
+
+
+
+
 
 
 ############### ROUND 2 ---- BETTER QA ###############
