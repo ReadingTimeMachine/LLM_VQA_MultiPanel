e9acb1039bbbdf03c105765c8549fdef5c1c5779
jnaiman <jnaiman.illinois.edu>
2026-05-08 12:02:55 -0500

    all replace names

---
+++ utils/replace_names_utils.py
@@ -0,0 +1,18 @@
+# replace_names = {'chatgpt_api':'ChatGPT', 
+#                         'chatgpt_api_highR_lowVerbose':'ChatGPT-HR',
+#                         #'chatgpt_api_highR_lowVerbose_mini':'ChatGPT-HR-mini', 
+#                         'chatgpt_api_highR_lowVerbose_mini':'ChatGPT-HRM', 
+#                         'gemini':'Gemini', 
+#                         'claude_haiku':'Claude', 
+#                         'claude_haiku_thinking':'Claude-T1',
+#                         'claude_haiku_thinking_maxT8000':'Claude-T'
+#                         }
+
+replace_names = {'chatgpt_api':'GPT', 
+                        'chatgpt_api_highR_lowVerbose':'GPT-HR',
+                        'chatgpt_api_highR_lowVerbose_mini':'GPT-HRM', 
+                        'gemini':'Gemini', 
+                        'claude_haiku':'Claude', 
+                        'claude_haiku_thinking':'Claude-T1',
+                        'claude_haiku_thinking_maxT8000':'Claude-T'
+                        }
